04e790ee0a4d3e8d773ab483786863a026350a9d
Kerminate <kepeilin@outlook.com>
2019-10-05 11:46:31 +0800

    feat: 增加城市选择浮层的搜索框

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -3,16 +3,24 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import './App.css';
 
+import CitySelector from '../common/CitySelector';
 import Header from '../common/Header';
 import DepartDate from './DepartDate';
 import HighSpeed from './HighSpeed';
 import Journey from './Journey';
 import Submit from './Submit';
 
-import { exchangeFromTo, showCitySelector } from './actions';
+import { exchangeFromTo, showCitySelector, hideCitySelector } from './actions';
 
 function App(props) {
-  const { from, to, dispatch } = props;
+  const {
+    from,
+    to,
+    isCitySelectorVisible,
+    cityData,
+    isLoadingCityData,
+    dispatch
+  } = props;
 
   const onBack = useCallback(() => {
     window.history.back();
@@ -25,21 +33,29 @@ function App(props) {
     }, dispatch);
   }, [dispatch]);
 
+  const citySelectorCbs = useMemo(() => {
+    return bindActionCreators({
+        onBack: hideCitySelector
+      }, dispatch);
+  }, [dispatch]);
+
   return (
     <div>
       <div className="header-wrapper">
         <Header title="火车票" onBack={onBack} />
       </div>
       <form className="form">
-        <Journey
-          from={from}
-          to={to}
-          {...cbs}
-          />
+        <Journey from={from} to={to} {...cbs} />
         <DepartDate />
         <HighSpeed />
         <Submit />
       </form>
+      <CitySelector
+        show={isCitySelectorVisible}
+        cityData={cityData}
+        isLoading={isLoadingCityData}
+        {...citySelectorCbs}
+      />
     </div>
   );
 }
