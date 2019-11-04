09381e49c79224683115dbc718e08f2f202df474
Kerminate <kepeilin@outlook.com>
2019-11-04 10:11:20 +0800

    feat: 补齐综合筛选组件所有功能

--- src/query/Bottom.jsx
+++ src/query/Bottom.jsx
@@ -2,6 +2,7 @@ import React, {
   memo,
   useState,
   useCallback,
+  useMemo,
 } from 'react';
 import PropTypes from 'prop-types';
 import classnames from "classnames";
@@ -148,13 +149,59 @@ const BottomModal = memo((props) => {
     }
   ];
 
+  const sure = () => {
+    setCheckedTicketTypes(localCheckedTicketTypes);
+    setCheckedTrainTypes(localCheckedTrainTypes);
+    setCheckedDepartStations(localCheckedDepartStations);
+    setCheckedArriveStations(localCheckedArriveStations);
+    setDepartTimeStart(localDepartTimeStart);
+    setDepartTimeEnd(localDepartTimeEnd);
+    setArriveTimeStart(localArriveTimeStart);
+    setArriveTimeEnd(localArriveTimeEnd);
+    toggleIsFiltersVisible();
+  };
+
+  const isResetDisabled = useMemo(() => {
+    return Object.keys(localCheckedTicketTypes).length === 0
+    && Object.keys(localCheckedTrainTypes).length === 0
+    && Object.keys(localCheckedDepartStations).length === 0
+    && Object.keys(localCheckedArriveStations).length === 0
+    && localDepartTimeStart === 0
+    && localDepartTimeEnd === 24
+    && localArriveTimeStart === 0
+    && localArriveTimeEnd === 24;
+  }, [
+    localArriveTimeEnd,
+    localArriveTimeStart,
+    localCheckedArriveStations,
+    localCheckedDepartStations,
+    localCheckedTicketTypes,
+    localCheckedTrainTypes,
+    localDepartTimeEnd,
+    localDepartTimeStart
+  ]);
+
+  const reset = () => {
+    if (isResetDisabled) return;
+    setLocalCheckedTicketTypes({});
+    setLocalCheckedTrainTypes({});
+    setLocalCheckedDepartStations({});
+    setLocalCheckedArriveStations({});
+    setLocalDepartTimeStart(0);
+    setLocalDepartTimeEnd(24);
+    setLocalArriveTimeStart(0);
+    setLocalArriveTimeEnd(24);
+  };
+
   return (
     <div className="bottom-modal">
       <div className="bottom-dialog">
         <div className="bottom-dialog-content">
           <div className="title">
-            <span className="reset">重置</span>
-            <span className="ok">确定</span>
+            <span className={classnames('reset', {
+              disabled: isResetDisabled
+            })} onClick={reset }>重置</span>
+            <span className="ok" onClick={sure}>确定</span>
           </div>
           <div className="options">
             {
@@ -237,6 +284,27 @@ const Bottom = (props) => {
     setArriveTimeEnd,
   } = props;
 
+  const noChecked = useMemo(() => {
+    return Object.keys(checkedTicketTypes).length === 0
+    && Object.keys(checkedTrainTypes).length === 0
+    && Object.keys(checkedDepartStations).length === 0
+    && Object.keys(checkedArriveStations).length === 0
+    && departTimeStart === 0
+    && departTimeEnd === 24
+    && arriveTimeStart === 0
+    && arriveTimeEnd === 24;
+  }, [
+    arriveTimeEnd,
+    arriveTimeStart,
+    checkedArriveStations,
+    checkedDepartStations,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    departTimeEnd,
+    departTimeStart
+  ]);
+
+
   return (
     <div className="bottom">
       <div className="bottom-filters">
@@ -259,10 +327,10 @@ const Bottom = (props) => {
           只看有票
         </span>
         <span
-          className={classnames("item", {'item-on': isFiltersVisible})}
+          className={classnames("item", {'item-on': isFiltersVisible || !noChecked})}
           onClick={toggleIsFiltersVisible}
         >
-          <i className="icon">{ '\uf0f7' }</i>
+          <i className="icon">{ noChecked ? '\uf0f7' : '\uf446' }</i>
           综合筛选
         </span>
       </div>
